f5b41a81e89927362e68ef995d93e07a4207c7d1
BIG KRIMPIN <bigkrimpin@protonmail.com>
2023-11-24 11:16:17 +0100

    Added wavr

--- Makefile
+++ Makefile
@@ -5,26 +5,31 @@ INCLUDES= \
 		  -Iexternal/PAL-CRT/ \
 
 CC=gcc
-CFLAGS=-std=c99 -Wall -Wextra -pedantic $(INCLUDES)
+CFLAGS=-std=c99 -Wall -Wextra -pedantic -lm -Ofast $(INCLUDES)
 LDFLAGS=-L/usr/lib
 
 OBJS= \
 	src/bk_pal_crt.o \
 
-all: bk_pal_crt.so 
+all: change_pal_system bk_pal_crt.so bk_wavr.so
 
 %.o: %.c
 	$(CC) -c -o $@ $< $(CFLAGS) -fpic
 
-bk_pal_crt.so: src/bk_pal_crt.o external/PAL-CRT/pal.o external/PAL-CRT/pal_core.o external/PAL-CRT/pal_nes.o external/PAL-CRT/pal_nesrgb.o
+change_pal_system:
 	# change PAL_SYSTEM from PAL_SYSTEM_NES_RGB to PAL_SYSTEM_PAL
 	sed -i -e 's/#define PAL_SYSTEM PAL_SYSTEM_NESRGB/#define PAL_SYSTEM PAL_SYSTEM_PAL/g' external/PAL-CRT/pal_core.h
+
+bk_pal_crt.so: src/bk_pal_crt.o external/PAL-CRT/pal.o external/PAL-CRT/pal_core.o external/PAL-CRT/pal_nes.o external/PAL-CRT/pal_nesrgb.o
+	$(CC) -shared -o $@ $^
+
+bk_wavr.so: src/bk_wavr.o
 	$(CC) -shared -o $@ $^
 
 .PHONY: clean install
 
 clean:
-	$(RM) bk_pal_crt.so src/*.o external/PAL-CRT/*.o
+	$(RM) bk_pal_crt.so bk_wavr.so src/*.o external/PAL-CRT/*.o
 
 install: bk_pal_crt.so
 	mkdir -p $(DESTDIR)$(PREFIX)/share/kdenlive/effects
---
+++ src/bk_wavr.c
@@ -0,0 +1,163 @@
+#include <float.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "frei0r.h"
+
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
+
+typedef struct wavr_instance {
+	unsigned int width;
+	unsigned int height;
+
+	double ampl;
+	double freq;
+	double shift;
+	double speed;
+	unsigned int count;
+} wavr_instance_t;
+
+int f0r_init()
+{
+	return 1;
+}
+
+void f0r_deinit()
+{}
+
+void f0r_get_plugin_info(f0r_plugin_info_t *wavr_info)
+{
+	wavr_info->name = "bk_wavr";
+	wavr_info->author = "BIG KRIMPIN";
+	wavr_info->plugin_type = F0R_PLUGIN_TYPE_FILTER;
+	wavr_info->color_model = F0R_COLOR_MODEL_RGBA8888;
+	wavr_info->frei0r_version = FREI0R_MAJOR_VERSION;
+	wavr_info->major_version = 0;
+	wavr_info->minor_version = 1;
+	wavr_info->num_params = 3;
+	wavr_info->explanation = "Makes the image do a wave";
+}
+
+f0r_instance_t f0r_construct(unsigned int width, unsigned int height)
+{
+	wavr_instance_t *inst = (wavr_instance_t*)calloc(1, sizeof(*inst));
+
+	inst->width = width;
+	inst->height = height;
+	inst->ampl = 50;
+	inst->freq = 50;
+	inst->shift = 1;
+	inst->speed = 1;
+
+	inst->count = 0;
+	
+	return (f0r_instance_t)inst;
+}
+
+void f0r_destruct(f0r_instance_t instance)
+{
+	free(instance);
+}
+
+void f0r_get_param_info(f0r_param_info_t *info, int param_index)
+{
+	switch(param_index)
+	{
+		case 0:
+			info->name = "amplitude";
+			info->type = F0R_PARAM_DOUBLE;
+			info->explanation = "";
+			break;
+		case 1:
+			info->name = "frequency";
+			info->type = F0R_PARAM_DOUBLE;
+			info->explanation = "";
+			break;
+		case 2:
+			info->name = "speed";
+			info->type = F0R_PARAM_DOUBLE;
+			info->explanation = "";
+			break;
+	}
+}
+
+void f0r_set_param_value(f0r_instance_t instance, f0r_param_t param,
+		int param_index)
+{
+	wavr_instance_t *inst = (wavr_instance_t*)instance;
+	switch(param_index)
+	{
+		case 0:
+			inst->ampl = *((double*)param);
+			break;
+		case 1:
+			inst->freq = *((double*)param);
+			break;
+		case 2:
+			inst->speed = *((double*)param);
+			break;
+	}
+	inst->shift = 0;
+}
+
+void f0r_get_param_value(f0r_instance_t instance, f0r_param_t param,
+		int param_index)
+{
+	wavr_instance_t *inst = (wavr_instance_t*)instance;
+	switch(param_index)
+	{
+		case 0:
+			*((double*)param) = inst->ampl;
+			break;
+		case 1:
+			*((double*)param) = inst->freq;
+			break;
+		case 2:
+			*((double*)param) = inst->speed;
+			break;
+	}
+}
+
+void f0r_update(f0r_instance_t instance, double time, const uint32_t *inframe,
+		uint32_t *outframe)
+{
+	wavr_instance_t *inst = (wavr_instance_t*)instance;
+	int w = inst->width;
+	int h = inst->height;
+	double offset = 0;
+	int offset_int;
+	
+	printf("\n%f\n", inst->shift);
+
+	memset(outframe, 0x00, sizeof(uint32_t) * w * h);
+
+	for (int x = 0; x < w; x++) {
+		offset = inst->ampl * sin((2.0 * M_PI * ((double) x + inst->shift)) / inst->freq);
+		offset_int = offset;
+		// printf("%d, %f\n", offset_int, offset);
+		for (int y = 0; y < h; y++) {
+			if (y + offset_int < 0 || y + offset_int >= h)
+				continue;
+			outframe[y * w + x] = inframe[(y + offset_int) * w + x];
+		}
+	}
+
+	for (int y = 0; y < 30; y++) {
+		for (int x = 0; x < 30; x++) {
+			if (inst->count % 3 == 0)
+				outframe[y * w + x] = 0xffff0000;
+			else if (inst->count % 3 == 1)
+				outframe[y * w + x] = 0xff00ff00;
+			else if (inst->count % 3 == 2)
+				outframe[y * w + x] = 0xff0000ff;
+		}
+	}
+
+	inst->count += 1;
+
+	inst->shift += 2.0;
+}
